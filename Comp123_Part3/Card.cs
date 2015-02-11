f15add4e45977bd67eec75246f1c75f9d2ab2303
akashruti <shruti_01@live.com>
2015-02-11 08:58:16 -0500

    Added Card class, Instance variable, Properties, Constructor

---
+++ Comp123_Part3/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_Part3
+{
+    class Card
+    {
+        // Instance Varaible
+        private string _face;
+        private string _suit;
+
+
+        // Properties
+        public string Face
+        {
+            get
+            {
+                return this._face; //Read only
+            }
+        }
+
+        public string Suit
+        {
+            get
+            {
+                return this._suit; //Read only
+            }
+        }
+
+        // Constructor
+        public Card(string face, string suit)
+        {
+            this._face = face;
+            this._suit = suit;
+        }
+    }
+}
---
+++ Comp123_Part3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_Part3
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
